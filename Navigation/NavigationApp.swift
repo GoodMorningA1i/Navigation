4eb038e2d0edff4969849d3e3672d7ab83f9d6c1
Ali Syed <alisyed0206@gmail.com>
2025-05-13 20:46:14 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Ali Syed on 2025-05-13.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
