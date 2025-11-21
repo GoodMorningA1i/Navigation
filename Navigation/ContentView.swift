46839828f2268070e15dcadf3018f65f95c21cde
Ali Syed <alisyed0206@gmail.com>
2025-11-21 17:29:23 -0500

    add saving feature to navigation stack

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,6 +7,38 @@
 
 import SwiftUI
 
+@Observable
+class PathStore {
+    var path: [Int] {
+        didSet {
+            save()
+        }
+    }
+
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
+                path = decoded
+                return
+            }
+        }
+
+        // Still here? Start with an empty path.
+        path = []
+    }
+
+    func save() {
+        do {
+            let data = try JSONEncoder().encode(path)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
 struct DetailView: View {
     var number: Int
     @Binding var path: [Int]
@@ -23,13 +55,13 @@ struct DetailView: View {
 }
 
 struct ContentView: View {
-    @State private var path = [Int]()
+    @State private var pathStore = PathStore()
 
     var body: some View {
-        NavigationStack(path: $path) {
-            DetailView(number: 0, path: $path)
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0, path: $pathStore.path)
                 .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $path)
+                    DetailView(number: i, path: $pathStore.path)
                 }
         }
     }
