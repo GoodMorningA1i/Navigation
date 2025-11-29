812204e1843ebe462b85159468eb936a8d9e8e32
Ali Syed <alisyed0206@gmail.com>
2025-11-28 22:22:25 -0500

    playing around with navigation bar customization, or at least the little that exists

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,62 +7,17 @@
 
 import SwiftUI
 
-@Observable
-class PathStore {
-    var path: [Int] {
-        didSet {
-            save()
-        }
-    }
-
-    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
-
-    init() {
-        if let data = try? Data(contentsOf: savePath) {
-            if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
-                path = decoded
-                return
-            }
-        }
-
-        // Still here? Start with an empty path.
-        path = []
-    }
-
-    func save() {
-        do {
-            let data = try JSONEncoder().encode(path)
-            try data.write(to: savePath)
-        } catch {
-            print("Failed to save navigation data")
-        }
-    }
-}
-
-struct DetailView: View {
-    var number: Int
-    @Binding var path: [Int]
-    
-    var body: some View {
-        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
-            .navigationTitle("Number: \(number)")
-            .toolbar {
-                Button("Home") {
-                    path.removeAll()
-                }
-            }
-    }
-}
-
 struct ContentView: View {
-    @State private var pathStore = PathStore()
-
     var body: some View {
-        NavigationStack(path: $pathStore.path) {
-            DetailView(number: 0, path: $pathStore.path)
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $pathStore.path)
-                }
+        NavigationStack {
+            List(0..<100) { i in
+                Text("Row \(i)")
+            }
+            .navigationTitle("Title goes here")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.blue)
+            .toolbarColorScheme(.dark, for: .navigationBar)
+            .toolbar(.hidden, for: .navigationBar)
         }
     }
 
